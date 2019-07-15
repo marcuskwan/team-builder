bf3a2901ec8bacae49b2fcb07adb2a93b78efd1e
Marcus Kwan <kwan.marcus@gmail.com>
2019-07-16 06:44:15 +1000

    added form inputs including select dropdown list

--- README.md
+++ README.md
@@ -49,10 +49,10 @@ x- Render your list of team members
 
 ### STEP 2 - Build your form
 
-- In `Form.js` build out your markup
-- Build inputs for `name`, `email` and `role` (backend engineer, frontend engineer, designer, etc. Use your imagination)
-- Import the useState hook and utilize what we learned about two-way data binding
-- Render your `Form` component in `App` and pass a setter method (the `set_____` method from your team members state in `App`) down to it so you can add team members to your state.
+x- In `Form.js` build out your markup
+x- Build inputs for `name`, `email` and `role` (backend engineer, frontend engineer, designer, etc. Use your imagination)
+x- Import the useState hook and utilize what we learned about two-way data binding
+x- Render your `Form` component in `App` and pass a setter method (the `set_____` method from your team members state in `App`) down to it so you can add team members to your state.
 
 ### STEP 3 - Add the edit functionality
 
--- team-builder/src/components/TeamForm.jsx
+++ team-builder/src/components/TeamForm.jsx
@@ -1,18 +1,37 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
 export default function TeamForm(props) {
-    return (
-        <div className="team-form">
-            <form>
-                <input
-                
-                />
-            </form>
-        </div>
-    )
+  const [memberData, setMemberData] = useState({
+    name: "",
+    email: "",
+    role: "",
+  });
+  return (
+    <div className="team-form">
+      <form>
+        <input
+          name="name"
+          value={memberData.name}
+          placeholder="name"
+          required
+        />
+        <input
+          name="email"
+          value={memberData.email}
+          placeholder="email"
+          type="email"
+          required
+        />
+      </form>
+      <select onChange={() => {}} value={{}}>
+        <option value="role">Roles</option>
+        <option value="Frontend Engineer">Frontend Engineer</option>
+        <option value="Backend Engineer">Backend engineer</option>
+        <option value="UI Designer">UI Designer</option>
+      </select>
+    </div>
+  );
 }
 
-TeamForm.propTypes = {
-
-}
+TeamForm.propTypes = {};
